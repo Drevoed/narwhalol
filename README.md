afd5f1d2e88dcec8bebb9a835da1996ec4421754
Drevoed <vetrokm@gmail.com>
2019-08-05 23:13:50 +0300

    readme and travis

---
+++ README.md
@@ -0,0 +1,20 @@
+# Narwhal
+
+***Narwhal*** is a Fast and Type-safe wrapper of DDragon and League of
+Legends API.
+
+It strives to provide the most comfortable and fast experience of
+getting useful data directly from Riot servers.
+
+Zero cost abstractions and compile-time optimizations used in Rust is
+what makes this library so fast.
+
+#### Benchmarks
+
+```rust
+```
+
+#### Examples
+
+```rust
+```
--- src/tests/api.rs
+++ src/tests/api.rs
@@ -8,6 +8,6 @@ lazy_static!{
 #[test]
 fn gets_summoner_data() {
     let summoner = LEAGUE_CLIENT.get_summoner_by_name("Santorin").expect("Something went wrong");
-    println!("Got summoner: {:?}", &summoner);
-    assert_eq!(&summoner.account_id, "CU8u5t6mieXsvcG5YdeVEsJictHK2is6QBO7mRJN1OaY-_c")
+    println!("Got summoner: {:#?}", &summoner);
+    assert_eq!(&summoner.account_id, "rPnj4h5W6OhejxB-AO3hLOQctgZcckqV_82N_8_WuCFdO2A")
 }
